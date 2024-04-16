75ab0c42f249e6de4fc6afd07c136f32d121ac75
Ariyadi <ariyadi.sosmed@gmail.com>
2024-04-16 20:57:21 +0800

    Model

---
+++ Model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+    public static void tampilanInfo(){
+        System.out.println();
+        System.out.println("=========== Data Mahasiswa ===========");
+        System.out.println("NIM : 2210010062");
+        System.out.println("Nama : Muhammad Ariyadi Perdana Putra");
+        System.out.println("======================================");
+    }
+}
